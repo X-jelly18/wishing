2167fec307f05826d374e587282b6868ec0b6b59
X-jelly18 <elitegamer128k@gmail.com>
2026-04-28 21:56:15 +0200

    Update backend URL in proxy handler

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "http://itachi.ayanakojixxx.shop:80" +
+    "http://sint.ayanakojivps.shop" +
     url.pathname +
     url.search;
 
